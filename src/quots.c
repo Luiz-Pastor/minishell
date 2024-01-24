af11341ef1996bef45e7f9d4de7d873aea04a504
Luiz-Pastor <luigpacr5@gmail.com>
2024-01-24 11:02:27 +0100

    Created a common function, to avoid repeated code

--- src/quots.c
+++ src/quots.c
@@ -2,26 +2,45 @@
 
 #define	QUOTE_MSG C_CYAN"dquote> "CLEAR
 
+static int	is_quot(char *input, int index)
+{
+	/* Miramos si es una comilla */
+	if (input[index] != '\'' && input[index] != '\"')
+		return (0);
+	
+	/* Si es simple, es o inicio o fin (no tiene escape posible) */
+	if (input[index] == '\'')
+		return (1);
+
+	/* Aqui es doble seguro, miramos si es el primer caracter o si tiene escape*/
+	return (input == 0 || input[index - 1] != '\\');
+}
+
 static char	*dquote(char *input)
 {
 	char	*new_line;
 	char	*inter;
 	char	*full;
 
-	new_line = readline(QUOTE_MSG);
+	/* Leemos linea: si devueve NULL (ctr+D), liberamos memoria */
+	new_line = readline("dquote> ");
 	if (!new_line)
 		return (ft_mfree(1, &input));
 
+	/* Metemos un salto de linea a la info que teniamos */
 	inter = ft_strjoin(input, "\n");
 	if (!inter)
 		return (ft_mfree(1, &input));
 
+	/* Juntamos la linea+\n con ell nuevo texto */
 	full = ft_strjoin(inter, new_line);
 	if (!full)
 		return (ft_mfree(2, &input, &new_line));
 
+	/* Liberamos la memoria auxiliar */
 	ft_mfree(3, &input, &inter, &new_line);
 
+	/* Devolvemos la linea anterior + \n + la nueva lectura */
 	return (full);
 }
 
@@ -29,16 +48,17 @@ static int	next_quot(char *input, int i)
 {
 	char	ch;
 
+	/* Nos guardamos el caracter po el que empieza la string, y buscamos el siguiente */
 	ch = input[i];
 	while (input[++i])
 	{
-		if (input[i] == ch && input[i] == '\'')
-			return (i);
-		if (input[i] == ch && input[i-1] != '\\' && ch == '\"')
-			return (i);
+		if (input[i] == ch && is_quot(input, i))
+			return i;
 	}
 	return (-1);
 }
+
+
 
 /*
 	- Id. A: al encontrar una comilla iterar hasta encontrar la otra
@@ -47,27 +67,35 @@ char	*check_quots(char *input)
 {
 	int	i;
 	int next;
-	
+
 	i = 0;
 	if (!input)
 		return (NULL);
 	while (input[i])
 	{
-		if ((input[i] == '\'' || input[i] == '\"') && (i == 0 || input [i-1] != '\\'))
+		/* Para cada caracter miramos si es un quot */
+		if (is_quot(input, i))
 		{
+			/*
+				Si es quot, buscamos la siguiente ocurrecia:
+					- Si no tiene ocurrencias, le pedimos al usuario mas texto y volvemos a comprobar
+					- Si encontramos ocurrencia, miramos mas quots a partir del quot que finaliza
+			*/
 			next = next_quot(input, i);
+
 			if (next == -1)
 			{
-				/* Pedir más input */
 				input = dquote(input); 
+				/* ctl+D a la hora de leer mas info, o error de memoria */
 				if (!input)
 					return (NULL);
-				i = -1;
+				i--;
 			}
 			else
 				i = next;
 		}
 		i++;
 	}
+	/* Devolvemos la linea con las quots bien puestas */
 	return (input);
 }
