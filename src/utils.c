ba31e8dc2e758c002e15aa34c57151548eb1ca48
Luiz-Pastor <luigpacr5@gmail.com>
2024-01-26 13:49:28 +0100

    Extracted a function

---
+++ src/utils.c
@@ -0,0 +1,10 @@
+#include "../inc/minishell.h"
+
+int	is_space(char ch)
+{
+	if (ch == ' ' || ch == '\n' || ch == '\t')
+		return (1);
+	if (ch == '\v' || ch == '\f' || ch == '\r')
+		return (1);
+	return (0);
+}
