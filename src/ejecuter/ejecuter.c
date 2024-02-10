491e1b2710c8c64007ef77d76e2b3b8ad9eb308a
Luiz-Pastor <luigpacr5@gmail.com>
2024-02-10 12:07:45 +0100

    Terminado el esquema

--- src/ejecuter/ejecuter.c
+++ src/ejecuter/ejecuter.c
@@ -1,18 +1,20 @@
 #include "../../inc/minishell.h"
 
 /* UN COMANDO
-Cosas a mirar para cada comando:
-	Crear un proceso hijo y:
-		- Abrir todos los infiles
-		- Abrimos todos los outfiles
+
+Primero mirar si es built-in o no:
+	- Si es un comando:
+		- Crear un proceso hijo
+		- Abrir todos los infiles y nos quedamos con el ultmo (o con stdin si no hay ninguno)
+		- Abrimos todos los outfiles y nos quedasmo con el ultimo (o con stdout si no hay ninguno)
 		- Sacar la ruta del comando
 		- Juntar comando con argumentos
 		- Hacer los dups
 		- Hacer execve
 
 Cosas a mirar en los build-ins
-	- Abrimos los infiles
-	- Abrimos los outfiles
+	- Abrir todos los infiles y nos quedamos con el ultmo (o con stdin si no hay ninguno)
+	- Abrimos todos los outfiles y nos quedasmo con el ultimo (o con stdout si no hay ninguno)
 	- Hacemos dup2
 	- Ejecutamos built-in
 	- Hacemos dup2 para volver a entrada y salida estandar
@@ -26,6 +28,19 @@ static void	one_command(t_msh *msh)
 		built_ins();
 	}
 }
+
+/* VARIOS COMANDOS
+
+Desde el padre, creamos tanto forks como comands haya.
+Para cada uno de los comandos:
+	- Abrir todos los infiles y nos quedamos con el ultmo (o con stdin si no hay ninguno)
+	- Abrimos todos los outfiles y nos quedasmo con el ultimo (o con stdout si no hay ninguno)
+	- Sacar la ruta del comando
+	- Juntar comando con argumentos
+	- Hacer los dups
+	- Hacer execve
+
+*/
 
 int	ejecuter(t_msh *msh)
 {
